25380d45b33f8dc195435a48f3a5a4bdc5647fef
Levin <dushiyi319@163.com>
2018-04-18 18:19:12 +0800

    update at 201804181819

--- script/simulate_fasta2fastq/simulate_fasta2fastq.py
+++ script/simulate_fasta2fastq/simulate_fasta2fastq.py
@@ -44,6 +44,8 @@ parser.add_argument('-v', action="version",dest="1.0",help='show version' )
 argument = parser.parse_args()
 
 def random_err(seq,sequence_length,err_rate):
+	if len(seq) != 100:
+		print("rawseq is not 100bp: %s"%seq)
 	i = int(err_rate*sequence_length)
 	# i 表示错误碱基个数 = 错误率 * 测序长度。比如：测序长度为100bp，那么10%的错误率则为10bp，即每100bp中最多可以有10bp错误。
 	err_num = random.randrange(i+1) 
@@ -52,31 +54,51 @@ def random_err(seq,sequence_length,err_rate):
 	if err_num == 0: # 当read中没有错误碱基时直接返回原始序列。
 		return seq,err_num
 	# err_num表示本条read的中将会有多少个错误碱基，
-	err_pos = [random.randrange(sequence_length)+1 for each in range(err_num)]
+	err_pos_1 = [random.randrange(sequence_length) for each in range(err_num)]
+	# print(type(err_pos_1[0]))
 	# err_pos 表示错误碱基的位置，需要随机出上面的err_num每个错误碱基的位置
-	# 这里sequence_length -1 表示从测序长度的最末端-1位开始随机 而最后 整体加1是为了不让其实位置从0开始，而直接起始位置从1开始。
+	# 这里从sequence_length随机出来的值是从0开始的，不包含最后一位，
 	# 从错误碱基中随机出一个列表，每个值表示错误碱基的位置。! 有重复,	
-	err_pos = list(set(err_pos))
+	err_pos = sorted(set(err_pos_1))
 	 # 对错误的位置列表进行去重并从小到大排序
-	# print("err_pos: %s"%err_pos)
-	# print("rawseq: %s"%seq) 
 	# 打印原始的read
-	random_base = [random.choice("ATCG") for i in range(err_num)] 
+	err_num = len(err_pos)
+	random_base = [random.choice("ATCG") for i in range(len(err_pos))] 
 	# 随机出每个位置的错误的碱基，用于替换原来的。这里有可能随机出来的还是原来正确的碱基。
 	# print("random_base: %s"%random_base)
+	seq2list=list(seq)
+	# 开始替换错误碱基，将str 格式的seq转成list格式，用列表的替换会更方便一些，
+	# err_pos = [0,2,99]
+	# random_base=['A','T','C']
+
 	for index,value in enumerate(err_pos): 
-	# 开始替换错误碱基，
-		newseq = seq[:value-1]+random_base[index]+seq[value:] 
+		seq2list[value] = random_base[index]
+		# 下面方法用字符串切片进行替换有时候会有bug，暂不明确
+		# newseq = seq[:value-1]+random_base[index]+seq[value:] 
 		# 这里需要测试两个极端，min和max，err_pos中的两个极端值为1和50（假设sequence_length=50）因为切片时的前端值是索引，但后端值切不到，所以真实位置需要-1，
 		#如果起始位置为1，那么索引中的0应该成为后端值，所以上面设置了value-1，后面部分value又代表了索引。
 		# 如果起始位置为50，那么，value-1=49,49切不上，只切了前48个，+随机值+seq[50:],50又不能成为索引,因为总共的索引是0-49。
-		seq = newseq
+		# seq = newseq
 		# print("newseq: %s"%newseq) 
 	#最后得到新的seq
+	newseq = ''.join(seq2list)
+	######################### debug ################################
+	# if len(err_pos_1) != len(err_pos):
+	if len(err_pos) != len(random_base):
+		# if 99 in err_pos or 0 in err_pos:
+		print("err_num: %s"%err_num)
+		print("err_pos_1: %s"%err_pos_1)
+		print("err_pos: %s"%err_pos)
+		print("random_base: %s"%random_base)
+		print("rawseq: %s"%seq) 
+		# print("seqist: %s"%seq2list)	
+		print("newseq: %s"%newseq) 
+	################################################################
 	return newseq,err_num
 for seq_record in SeqIO.parse(argument.ref,"fasta"):
 	fasta_seq = seq_record.seq
 	fasta_length = len(seq_record.seq)
+	# print(fasta_length)
 # print(type(fasta_seq))
 
 sequence_type = argument.seq_type[:2]
@@ -84,7 +106,7 @@ sequence_length = int(argument.seq_type[2:])
 
 # result_list = [random.randrange(10) for i in range(100)] 
 # 表示随机生成100个（0到10）之间的随机数，其中0包含在里面，10不包含在里面，并且随机数之间会有重复。从0开始也表示索引，
-result_list = [random.randrange(fasta_length+1-sequence_length+1) for i in range(argument.data_amount)]
+result_list = [random.randrange(fasta_length+1-sequence_length) for i in range(argument.data_amount)]
 # 这里应该减去测序长度才行，否则，一旦随机出来的数值落在最后的测序长度范围内时，就没办法截取足够长的片段。
 # 表示随机生成5000000个（0到fasta_length+1-sequence_length）之间的随机数，并且随机数之间可能会有重复。随机出来的值的最小值为0，最大值为fasta_length+1-sequence_length+1，推理过程见下面
 # 但是如何才能让随机数具有均一性，或者能平均的覆盖到整个ref上？
@@ -96,7 +118,7 @@ if sequence_type == 'SE':
 			seq = fasta_seq[i:i+int(sequence_length)]
 			# 上一行代码表示：字符串切片是截取首尾字符的索引。冒号前一位包含，冒号后一位不包含，比如：[2:8]，2截取时第二位包含，8截取时第8位不包含在里面。
 			# 那么就需要 i+sequence_length的最大值为fasta_length+1，这样才能保证，ref的最后1bp不被漏掉。那么就需要i=fasta_length+1-sequence_length.
-			# 此处的i是随机出来的，但是产生随机数时，random.randrange(x),此处的x是不包括x本身的，那么就需要产生随机数时的x = i+1 =fasta_length+1-sequence_length+1.
+			# 此处的i是随机出来的，但是产生随机数时，random.randrange(x),此处的x是索引，那么x = i = fasta_length+1-sequence_length.
 			seq_with_err,err_num = random_err(seq,sequence_length,argument.err_rate)
 
 			f.write("@read_%s%s_%s_E%s/1\n"%(sequence_type,sequence_length,i,err_num))
